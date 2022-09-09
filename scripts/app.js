c6792114621bff88e50be229a3af57adbe802998
HartJN <jameshart04@gmail.com>
2022-09-09 16:59:00 +1200

    add mobile nav

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////
+// Mobile Nav
+
+const btnNavEl = document.querySelector('.btn-mobile-nav')
+const headerEl = document.querySelector('.header')
+const nav = document.querySelector('nav')
+const contactEl = document.querySelector('.contact')
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open')
+  if (contactEl !== null) contactEl.classList.toggle('disable-display')
+
+  nav.addEventListener('click', () => {
+    headerEl.classList.remove('nav-open')
+    if (contactEl !== null) contactEl.classList.toggle('disable-display')
+  })
+})
