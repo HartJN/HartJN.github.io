1dc7e3c8d31374cf8b0fc5fd57c3409aa08ab450
HartJN <jameshart04@gmail.com>
2022-09-16 09:42:56 +1200

    add event listener to change image

--- scripts/app.js
+++ scripts/app.js
@@ -24,7 +24,12 @@ document.oncontextmenu = function () {
 }
 
 const exampleBtnEl = document.querySelector('.btn-example')
+const img = document.querySelector('.heart-img')
 
-exampleBtnEl.addEventListener('click', () => {
-  exampleBtnEl.classList.toggle('btn-invert')
+img.addEventListener('click', function () {
+  if (this.srcset === '/blog/imgs/heart-outline-noBG.webp') {
+    this.srcset = '/blog/imgs/heart-colour-noBG.webp'
+  } else {
+    this.srcset = '/blog/imgs/heart-outline-noBG.webp'
+  }
 })
