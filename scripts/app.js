ced798993fce17e99e0a27f68bf17804e3b36f7e
HartJN <jameshart04@gmail.com>
2022-09-25 14:42:58 +1300

    add hover handler

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 ///////////////////////////////////////////////////////////
 // Mobile Nav
 
@@ -5,6 +7,7 @@ const btnNavEl = document.querySelector('.btn-mobile-nav')
 const headerEl = document.querySelector('.header')
 const nav = document.querySelector('nav')
 const navMenuEl = document.querySelectorAll('.nav__list')
+const homeRightEl = document.querySelector('.home-right')
 
 btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open')
@@ -15,3 +18,18 @@ btnNavEl.addEventListener('click', function () {
 document.oncontextmenu = function () {
   return true
 }
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+    const logo = link.closest('.header').querySelector('.logo')
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this
+    })
+    logo.style.opacity = this
+  }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5))
+nav.addEventListener('mouseout', handleHover.bind(1))
